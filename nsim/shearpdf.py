5692c5dd44ed15dc78cc704b0f180f5425edc7e8
Erin Sheldon <erin.sheldon@gmail.com>
2015-10-13 18:42:16 -0700

    shear pdf classes

---
+++ nsim/shearpdf.py
@@ -0,0 +1,24 @@
+import random
+from ngmix import Shape
+
+class ShearGeneratorBase(object):
+    def get_shear(self):
+        raise NotImplementedError("implement get_shear()")
+
+class ConstShearGenerator(ShearGeneratorBase):
+    def __init__(self, shears):
+
+        if not isinstance(shears[0], list):
+            shears = [shears]
+
+        shears = [Shape(s[0],s[1]) for s in shears]
+
+        self.nshear=len(shears)
+        self.shears=shears
+
+    def get_shear(self):
+        """
+        return a random shear from the input list, plus an index
+        """
+        ri = random.randint(0, self.nshear-1)
+        return self.shears[ri], ri

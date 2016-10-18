4fe4ee0d0c55027fdb0cafe56ec47117a9333a4c
Erin Sheldon <erin.sheldon@gmail.com>
2016-10-18 10:11:09 -0400

    new joint r50-flux pdf
    use a kde to represent the cosmos sample

--- nsim/pdfs.py
+++ nsim/pdfs.py
@@ -1,5 +1,9 @@
+from __future__ import print_function
+import os, sys
+import fitsio
 import numpy
 import esutil as eu
+from esutil.numpy_util import between
 
 class DiscreteSampler(object):
     def __init__(self, vals, rng=None):
@@ -46,3 +50,87 @@ class PowerLaw(object):
 
     def sample(self, n=None):
         return self.pdf.sample(n)
+
+
+class CosmosR50Flux(object):
+    def __init__(self, r50_range, flux_range):
+        self.r50_range=r50_range
+        self.flux_range=flux_range
+
+        self.r50_sanity_range=0.05,2.0
+        self.flux_sanity_range=0.5,100.0
+        self.kde_factor=0.01
+
+        self._load_data()
+        self._make_kde()
+
+    def sample(self, size=None):
+        """
+        get [r50, flux] or [:, r50_flux]
+        """
+        if size is None:
+            size=1
+            is_scalar=True
+        else:
+            is_scalar=False
+
+        r50min,r50max=self.r50_range
+        fmin,fmax=self.flux_range
+
+        data=numpy.zeros( (size,2) )
+
+        ngood=0
+        nleft=data.shape[0]
+        while nleft > 0:
+            r=self.kde.resample(size=nleft).T
+
+            w,=numpy.where(
+                between(r[:,0], r50min, r50max) &
+                between(r[:,1], fmin, fmax)
+            )
+
+            if w.size > 0:
+                data[ngood:ngood+w.size,:] = r[w,:]
+                ngood += w.size
+                nleft -= w.size
+
+        if is_scalar:
+            data=data[0,:]
+
+        return data
+
+    def _load_data(self):
+        fname='real_galaxy_catalog_25.2_fits.fits'
+        fname=os.path.join(
+            sys.exec_prefix,
+            'share',
+            'galsim',
+            'COSMOS_25.2_training_sample',
+            fname,
+        )
+
+        r50min,r50max=self.r50_sanity_range
+        fmin,fmax=self.flux_sanity_range
+
+        print("reading cosmos file:",fname)
+        alldata=fitsio.read(fname, lower=True)
+        w,=numpy.where(
+            (alldata['viable_sersic']==1) &
+            between(alldata['hlr'][:,0], r50min, r50max) &
+            between(alldata['flux'][:,0], fmin, fmax)
+        )
+        print("kept %d/%d" % (w.size, alldata.size))
+
+        self.alldata=alldata[w]
+
+    def _make_kde(self):
+        import scipy.stats
+
+        data=numpy.zeros( (self.alldata.size, 2) )
+        data[:,0] = self.alldata['hlr'][:,0]
+        data[:,1] = self.alldata['flux'][:,0]
+
+        self.kde=scipy.stats.gaussian_kde(
+            data.transpose(),
+            bw_method=self.kde_factor,
+        )

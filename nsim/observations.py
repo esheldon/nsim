475d9d9aad3f3f49805d772d03931efb5462e0e4
Erin Sheldon <erin.sheldon@gmail.com>
2018-02-04 06:25:37 -0800

    remove trim from coaddsim branch

--- nsim/observations.py
+++ nsim/observations.py
@@ -208,9 +208,10 @@ class ObservationMaker(dict):
             **kw
         )
 
-        trim_dims=coadd_conf.get('trim_dims',None)
+        #trim_dims=coadd_conf.get('trim_dims',None)
         if coadd_conf['type']=='mean':
-            coadd_obs = coadder.get_mean_coadd(dims=trim_dims)
+            #coadd_obs = coadder.get_mean_coadd(dims=trim_dims)
+            coadd_obs = coadder.get_mean_coadd()
         else:
             raise ValueError("bad coadd type: '%s'" % coadd_conf['type'])
 
